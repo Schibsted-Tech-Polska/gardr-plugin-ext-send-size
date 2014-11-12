4e0ce630e1441b54dea5f12a3e06bca671f2fc85
Krystian Jarmicki <jarmicki@gmail.com>
2014-11-12 14:10:54 +0100

    handling <object>

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var xde = require('cross-domain-events');
 var childrenSize = require('../gardr-ext/lib/childrensize.js');
 
 var gardrSendSize = function() {
+    
+    if(typeof global.document.contentWindow === 'undefined') {
+        global.document.contentWindow = global;
+    }
 
     xde.on('plugin:send-size', function() {
         var size = childrenSize(global.gardr.container);
